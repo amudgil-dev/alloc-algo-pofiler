8c6ed62c8148776f98786861f3d76d8240f6cdfc
Aadya Mudgil <aadyamudgil1@gmail.com>
2024-10-30 22:34:19 +0000

    removed duplicate scripts

--- scripts copy/deactivate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-deactivate
-echo "Virtual Env de-activated"
--- scripts copy/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source venv/bin/activate
-
-
-# Run the application
-echo " - Starting simulation "
-python3 hetroPlot.py
